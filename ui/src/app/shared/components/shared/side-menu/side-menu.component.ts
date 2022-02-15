f76166a13e97cee39d080c5a33a28b2e1316d5ac
Thangavel Ayyasamy <rpdigitaltirupur@gmail.com>
2022-02-15 23:10:09 +0530

    Github Pages

--- ui/src/app/shared/components/shared/side-menu/side-menu.component.ts
+++ ui/src/app/shared/components/shared/side-menu/side-menu.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { IMenu } from '../../../../core/models/common/Menu';
+import { ILanguage } from '../../../../core/models/common/Language';
+import menu from '../../../../../assets/files/json/menu.json';
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  styleUrls: ['./side-menu.component.scss'],
 })
 export class SideMenuComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  navList: IMenu[] = [];
+  profileList: IMenu[] = [];
+  @Input() selectedLang!: ILanguage;
+  constructor() {
+    this.navList = menu.menuList;
+    this.profileList = menu.profileList;
   }
 
+  ngOnInit(): void {}
 }
